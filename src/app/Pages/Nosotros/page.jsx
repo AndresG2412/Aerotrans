183881fb9621e4d39669fbecebd32eee9105c27f
Andres Gaviria <cgaviria930@gmail.com>
2024-11-14 22:52:37 -0500

    avances Pagina Nosotros

--- src/app/Pages/Nosotros/page.jsx
+++ src/app/Pages/Nosotros/page.jsx
@@ -1,7 +1,39 @@
+import Bento from "../../components/Bento";
+
 export default function Nosotros() {
     return(
-        <>
-            Pagina de nosotros
+        <>  
+            <div className="max-w-7xl mx-auto mt-36">
+                <div className="flex items-center mb-4">
+                    <hr className="flex-grow border-[3px] border-black"/>
+                    <p className="text-4xl font-bold tracking-wide mx-4">MISION</p>
+                    <hr className="flex-grow w-9/12 border-[3px] border-black"/>
+                </div>
+
+
+                <div className="font-semibold text-xl border-4 border-black p-6 bg-white shadow-2xl">
+                    Somos una empresa del sector cooperativo dedicada al servicio del transporte de pasajeros orgullosamente Laboyana; 
+                    trabajamos con mística, en equipo y por competencias laborales, para satisfacer las necesidades y expectativas de 
+                    nuestros clientes, prestando un servicio con seguridad, calidad, y cumplimiento, para agrado de nuestros asociados 
+                    y sociedad en general.
+                </div>
+            </div>
+
+            <div className="max-w-7xl mx-auto mt-16">
+                <div className="flex items-center mb-4">
+                    <hr className="flex-grow w-9/12 border-[3px] border-black"/>
+                    <p className="text-4xl font-bold tracking-wide mx-4">VISION</p>
+                    <hr className="flex-grow border-[3px] border-black"/>
+                </div>
+
+
+                <div className="font-semibold text-xl border-4 border-black p-6 bg-white shadow-2xl">
+                Ser lideres en la región en el sector transporte en las diferentes modalidades que préstamos, aplicando innovación 
+                constante, tecnología avanzada, respetuosos 
+                con nuestros usuarios y el medio ambiente, mejorando las necesidades de sus grupos de interés; con un talento humano 
+                competente, siendo modelos en experiencia y solidez.
+                </div>
+            </div>
         </>
     );
 }
---
+++ src/app/components/Bento.jsx
@@ -0,0 +1,10 @@
+export default function Bento() {
+    return (
+        <div className="mt-40 grid grid-cols-4 gap-4 bg-gray-100 p-4 rounded-lg shadow-lg max-w-6xl mx-auto">
+            <div class="col-span-1 bg-red-500 rounded text-center">01</div>
+            <div class="col-span-1 bg-blue-500 rounded text-center">02</div>
+            <div class="col-span-1 bg-purple-500 rounded text-center">03</div>
+            <div class="col-span-1 bg-green-500 rounded text-center">04</div>
+        </div>
+    );
+}
