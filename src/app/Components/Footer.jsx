3f6ab2561da7d89c8009a412c4dc5e763356c431
Andres Gaviria <cgaviria930@gmail.com>
2024-11-21 20:05:08 -0500

    footer modelo 1

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -4,23 +4,48 @@ import Image from "next/image";
 import img1 from "../Images/footer/laf.png";
 import fb from "../Images/footer/fb.png";
 import twt from "../Images/footer/twt.png";
-import inst from "../Images/footer/inst.png";
+import inst from "../Images/footer/inst.png";   
 
 export default function Footer() {
     return (
-        <>
-            <div className="w-screen h-auto bg-gray-800 flex mt-20">
-                <div className="w-10/12 mx-auto flex justify-between items-center py-8">
+        <footer
+            className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
+            <div
+                className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-neutral-500 lg:justify-between">
+                <div className="mr-12 hidden lg:block">
+                    <span className="font-semibold tracking-wide">Mantente conectado con nosotros siempre!</span>
+                </div>
+            </div>
 
-                    <Image priority={false} src={img1} alt="logo_footer" className="w-96" />
+            {/* <!-- Main container div: holds the entire content of the footer, including four sections (TW Elements, Products, Useful links, and Contact), with responsive styling and appropriate padding/margins. --> */}
+            <div className="mx-auto my-12 w-3/4 ">
+                <div className="flex justify-between">
+                    <div className="">
+                        <Image priority={false} className="" src={img1} alt="Logo"/>
+                    </div>
 
-                    <div className="flex items-center">
-                        <Image src={fb} alt="icon_fb" className="w-[75px] h-[75px] ml-12" />
-                        <Image src={twt} alt="icon_twt" className="w-[75px] h-[75px] ml-12" />
-                        <Image src={inst} alt="icon_inst" className="w-[75px] h-[75px] ml-12" />
+                    <div className="my-auto">
+                        <h6 className="mb-8 font-bold uppercase text-center text-2xl tracking-widest">
+                            Contactos
+                        </h6>
+
+                        <div className="flex gap-10 justify-center">
+                            <Image priority={false} className=" w-16" src={fb} alt="Logo"/>
+                            <Image priority={false} className=" w-16" src={twt} alt="Logo"/>
+                            <Image priority={false} className=" w-16" src={inst} alt="Logo"/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </>
+
+            {/* <!--Copyright section--> */}
+            <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
+                <span>© 2024 Copyright:</span>
+                <a
+                className="font-semibold text-neutral-600 dark:text-neutral-400"
+                href="https://tw-elements.com/"
+                > Aerotrans LTDA</a>
+            </div>
+        </footer>
     );
 }
--- src/app/Pages/Contacto/page.jsx
+++ src/app/Pages/Contacto/page.jsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import b_c from "@/app/Images/Contacto/b_c.png"
+
 export default function Contacto() {
     return(
-        <>
-            Pagina de contacto
-        </>
+        <div className="mt-36">
+            <Image src={b_c} alt="Rectangulo_1" className="w-full h-[500px]" />
+        </div>
     );
 }
