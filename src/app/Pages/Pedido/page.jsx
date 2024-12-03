46128c15f0c5d494bb8ebde6d0790c615ed6c351
Andres Gaviria <cgaviria930@gmail.com>
2024-12-02 19:03:56 -0500

    inicio formulario pedido

--- src/app/Pages/Pedido/page.jsx
+++ src/app/Pages/Pedido/page.jsx
@@ -1,7 +1,44 @@
-export default function Pedido() {
-    return(
-        <>
-            pedidos por la pagina
-        </>
+"use client";
+
+import { useForm } from "react-hook-form";
+import Button from "@/app/Components/Botton";
+import enviar from "@/app/Images/Contacto/enviar.png"
+
+export default function App() {
+
+    const { register } = useForm();
+
+    return (
+        <form className="mt-40 w-2/3 px-6 rounded-xl py-12 mx-auto formulario">
+            <div className="form__group field">
+                <input type="input" className="form__field" placeholder="Name" required/>
+                <label for="nombre" className="form__label">Nombre Completo</label>
+            </div>
+
+            <div className="form__group field">
+                <input type="tel" className="form__field" placeholder="Name" required/>
+                <label for="telefono" className="form__label">Telefono</label>
+            </div>
+
+            <div className="form__group field">
+                <input type="tel" className="form__field" placeholder="Name" required/>
+                <label for="direccion" className="form__label">Direccion</label>
+            </div>
+
+            <div className="form__group field mt-4">
+            <label for="equipaje" className="form__label">Equipaje Pesado</label>
+                <div className="form__options mt-4 flex gap-10">
+                    <label>
+                    <input type="radio" name="equipaje" value="si" required /> Sí
+                    </label>
+                    <label>
+                    <input type="radio" name="equipaje" value="no" required /> No
+                    </label>
+                </div>
+            </div>
+
+            <Button titulo={"Enviar"} color={"green"} image={enviar}></Button>
+
+        </form>
     );
 }
