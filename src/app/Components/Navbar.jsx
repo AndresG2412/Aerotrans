6c69b22fffb77ea017d2b8abf6c2b7994feb46d8
Andres Gaviria <cgaviria930@gmail.com>
2025-02-09 18:10:42 -0500

    cambios navbar responsible

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -17,28 +17,28 @@ export default function Navbar() {
     };
 
     return (
-        <nav className="bg-white shadow-xl w-[100%]">
+        <nav className="bg-white shadow-xl w-[100%] fixed top-0 right-0 z-50">
             <ul className="w-[100%] flex justify-end items-center">
                 <li className="h-[60px] mr-auto">
-                    <Link className="h-[100%] px-[30px] flex items-center color-black text-xl font-bold tracking-wider hover:bg-[#f0f0f0] hover:transition-all" href="/">
+                    <Link className="h-[100%] px-[30px] flex items-center" href="/">
                         <Image priority={false} className="h-9 w-auto imagenes" src={logo} alt="Logo"/>
                         <Image priority={false} className="h-9 w-auto hidden show-image" src={logosm} alt="Logo"/>
                     </Link>
                 </li>
                 <li className="h-[60px]">
-                    <Link className="h-[100%] px-[20px] hide-on-small flex items-center color-black text-xl font-bold tracking-wider hover:bg-[#f0f0f0] hover:transition-all" href="../Pages/Pedido">PEDIR TAXI</Link>
+                    <Link className="h-[100%] px-[20px] hide-on-small flex items-center color-black text-xl font-bold tracking-wider hover:border-[#d4d4d4] hover:duration-[400ms] border-white border-b-4 hover:transition-all" href="../Pages/Pedido">PEDIR TAXI</Link>
                 </li>
                 <li className="h-[60px]">
-                    <Link className="h-[100%] px-[20px] hide-on-small flex items-center color-black text-xl font-bold tracking-wider hover:bg-[#f0f0f0] hover:transition-all" href="../Pages/Politicas">POLITICAS</Link>
+                    <Link className="h-[100%] px-[20px] hide-on-small flex items-center color-black text-xl font-bold tracking-wider hover:border-[#d4d4d4] hover:duration-[400ms] border-white border-b-4 hover:transition-all" href="../Pages/Politicas">POLITICAS</Link>
                 </li>
                 <li className="h-[60px]">
-                    <Link className="h-[100%] px-[20px] hide-on-small flex items-center color-black text-xl font-bold tracking-wider hover:bg-[#f0f0f0] hover:transition-all" href="../Pages/Nosotros">NOSOTROS</Link>
+                    <Link className="h-[100%] px-[20px] hide-on-small flex items-center color-black text-xl font-bold tracking-wider hover:border-[#d4d4d4] hover:duration-[400ms] border-white border-b-4 hover:transition-all" href="../Pages/Nosotros">NOSOTROS</Link>
                 </li>
                 <li className="h-[60px]">
-                    <Link className="h-[100%] px-[20px] hide-on-small flex items-center color-black text-xl font-bold tracking-wider hover:bg-[#f0f0f0] hover:transition-all" href="../Pages/Contacto">CONTACTO</Link>
+                    <Link className="h-[100%] px-[20px] hide-on-small flex items-center color-black text-xl font-bold tracking-wider hover:border-[#d4d4d4] hover:duration-[400ms] border-white border-b-4 hover:transition-all" href="../Pages/Contacto">CONTACTO</Link>
                 </li>
                 <li className="h-[60px]">
-                    <Link className="h-[100%] px-[20px] hide-on-small flex items-center color-black text-xl font-bold tracking-wider hover:bg-[#f0f0f0] hover:transition-all" href="../Pages/Servicios">SERVICIOS</Link>
+                    <Link className="h-[100%] px-[20px] hide-on-small flex items-center color-black text-xl font-bold tracking-wider hover:border-[#d4d4d4] hover:duration-[400ms] border-white border-b-4 hover:transition-all" href="../Pages/Servicios">SERVICIOS</Link>
                 </li>
                 <li className="h-[60px]">
                     <button onClick={toggleMenu} className="h-[100%] px-[30px] hidden show-burger items-center">
@@ -48,9 +48,9 @@ export default function Navbar() {
             </ul>
 
             {/* Menú desplegable */}
-            <ul className={`menu-dropdown backdrop-blur-xs bg-[rgba(255,255,255,0.2)] items-start fixed top-0 right-0 h-[100vh] z-50 shadow-xl flex-col justify-start ${isMenuOpen ? 'flex' : 'hidden'}`}>
+            <ul className={`menu-dropdown backdrop-blur-xs bg-[rgba(255,255,255)] items-start fixed top-0 right-0 h-[100vh] z-50 shadow-xl flex-col justify-start ${isMenuOpen ? 'flex' : 'hidden'}`}>
                 <li className="h-[60px] w-[100%]">
-                    <button onClick={toggleMenu} className="w-[100%] h-[100%] px-[30px] flex items-center justify-end hover:bg-[#f0f0f0] hover:transition-all">
+                    <button onClick={toggleMenu} className="w-[100%] h-[100%] px-[30px] flex items-center justify-end">
                         <Image priority={false} className="w-7 h-auto" src={close} alt="Close"/>
                     </button>
                 </li>
