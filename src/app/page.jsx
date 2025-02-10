2e9a5b86a2a7e356b268bb799023c060c3285708
Andres Gaviria <cgaviria930@gmail.com>
2025-02-10 14:28:22 -0500

    inicio main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,79 +18,31 @@ const images = [s1, s2, s3]
 export default function Home() {
 	return (
 		<>
-			<h1 className="font-semibold text-2xl text-center mt-36">La calidad total a su servicio</h1>
-            <h1 style={{ fontFamily: "'Inknut Antiqua', serif" }} className="font-bold text-8xl text-center mt-6">AEROTRANS</h1>
-
-            <div className="relative w-full mt-24 h-[500px] bg-cover bg-center overflow-hidden" 
-                style={{ backgroundImage: `url(${img1.src})` }} >
-                <div className="absolute inset-0 flex items-center justify-center">
-                    <div className="mr-10">
-                        <p className="font-bold text-3xl my-6">Con muchos servicios</p>
-                        <p className="font-bold text-3xl my-6">disponibles para cada</p>
-                        <p className="font-bold text-3xl my-6">necesidad y con seguridad</p>
-                    </div>
+            <h1 className="font-semibold text-xl md:text-2xl text-center mt-36">La calidad total a su servicio</h1>
+            <h1 style={{ fontFamily: "'Inknut Antiqua', serif" }} className="font-bold text-6xl md:text-8xl text-center mt-3">AEROTRANS</h1>
+		
+            <div 
+                className="mt-16 h-auto w-full md:h-64 md:flex justify-center items-center gap-x-32" 
+                style={{ 
+                    backgroundImage: `url(${img1.src})`, 
+                    backgroundSize: 'cover', 
+                    backgroundPosition: 'center', 
+                    backgroundRepeat: 'no-repeat' 
+                }}
+            >
+                <div className="text-center md:text-left pt-12 md:p-0">
+                    <p className="font-semibold text-xl md:text-3xl my-3 md:my-6">Con muchos servicios</p>
+                    <p className="font-semibold text-xl md:text-3xl my-3 md:my-6">disponibles para cada</p>
+                    <p className="font-semibold text-xl md:text-3xl my-3 md:my-6">necesidad y con seguridad</p>
+                </div>
+                <div className="pb-12 md:p-0">
                     <Image 
                         src={servicios} 
                         alt="servicios" 
-                        className="ml-10 max-w-full object-contain"
+                        className="mt-8 md:mt-0 mx-auto"
                     />
                 </div>
             </div>
-
-            <div className="flex justify-center items-center mt-24">
-                <div className="mr-24">
-                    <Slider images={images}/>
-                </div>
-
-                <div className="tracking-wide text-center ml-24">
-                    <p className="text-4xl my-6 font-bold text-black">Manejando controles</p>
-                    <p className="text-4xl my-6 font-bold text-black">de seguridad antes</p>
-                    <p className="text-4xl my-6 font-bold text-black">de cada jornada</p>
-                </div>
-            </div>
-
-            <div className="flex justify-center items-center mt-24">
-                <div className="tracking-wide text-center mr-24">
-                    <p className="text-4xl my-6 font-bold text-black">Antes de cada jornada</p>
-                    <p className="text-4xl my-6 font-bold text-black">verificando la seguridad</p>
-                    <p className="text-4xl my-6 font-bold text-black">para todos los pasajero</p>
-                </div>
-
-                <div className="ml-24">
-                    <Slider images={images}/>
-                </div>
-            </div>
-
-            <div className="flex justify-center items-center mt-24">
-                <div className="mr-24">
-                    <Slider images={images}/>
-                </div>
-
-                <div className="tracking-wide text-center ml-24">
-                    <p className="text-4xl my-6 font-bold text-black">Y asi finalmente</p>
-                    <p className="text-4xl my-6 font-bold text-black">poder brindarte</p>
-                    <p className="text-4xl my-6 font-bold text-black">la mejor experiencia</p>
-                </div>
-            </div>
-
-            <div className="mt-24">
-                <p className="font-bold text-2xl text-center">CALIFICA TU EXPERIENCIA</p>
-                <div className="mt-4 border-4 border-black rounded-xl w-[45rem] h-auto mx-auto flex flex-col p-6">
-                    <div className="max-w-full flex justify-center gap-16 mb-6">
-                        <div>
-                            <input type="text" placeholder="Nombre completo" className="py-2 px-4 rounded-lg bg-transparent shadow-xl border-2 border-dashed border-[#0000001a] block my-4"/>
-                            <input type="email" placeholder="Correo electronico" className="py-2 px-4 rounded-lg bg-transparent shadow-xl border-2 border-dashed border-[#0000001a] block my-4"/>
-                            <div className="mt-8">
-                                <Rating/>
-                            </div>
-                        </div>
-                        <ImageUpload/>
-                    </div>
-
-                    <textarea id="message" rows="4" className="mx-auto w-3/4 resize-none py-2 px-4 rounded-lg bg-transparent shadow-2xl border-2 border-dashed border-[#0000001a]" placeholder="Escribenos tu experiencia"></textarea>
-                    <Button titulo={"Enviar"} color={"red"} image={enviar}></Button>
-                </div>
-            </div>
-		</>
+        </>
 	);
 }
