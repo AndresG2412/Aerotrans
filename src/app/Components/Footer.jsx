eed36172ed0e8c2bfc6260679b512267a8f22c37
Andres Gaviria <cgaviria930@gmail.com>
2025-02-15 21:40:21 -0500

    cambios footer

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -2,16 +2,15 @@
 
 import Image from "next/image";
 import img1 from "../Images/footer/laf.png";
-import fb from "../Images/footer/fb.png";
-import twt from "../Images/footer/twt.png";
-import inst from "../Images/footer/inst.png";   
+
+import Social from "./Social";
 
 export default function Footer() {
     return (
         <footer
-            className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left mt-36">
+            className="bg-neutral-100 text-center text-neutral-600 dark:bg-[#121825] dark:text-neutral-200 lg:text-left mt-36">
             <div
-                className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-neutral-500 lg:justify-between">
+                className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-[#555] lg:justify-between">
                 <div className="mr-12 hidden lg:block">
                     <span className="font-semibold tracking-wide">Mantente conectado con nosotros siempre!</span>
                 </div>
@@ -33,16 +32,14 @@ export default function Footer() {
                         </h6>
 
                         <div className="flex gap-10 justify-center">
-                            <Image priority={false} className=" w-16" src={fb} alt="Logo"/>
-                            <Image priority={false} className=" w-16" src={twt} alt="Logo"/>
-                            <Image priority={false} className=" w-16" src={inst} alt="Logo"/>
+                            <Social />
                         </div>
                     </div>
                 </div>
             </div>
 
             {/* <!--Copyright section--> */}
-            <div className="bg-neutral-200 p-6 text-center dark:bg-neutral-700">
+            <div className="bg-neutral-200 p-6 text-center dark:bg-[#121825] border-[#555] border-t-2">
                 <span>© 2024 Copyright:</span>
                 <a
                 className="font-semibold text-neutral-600 dark:text-neutral-400"
