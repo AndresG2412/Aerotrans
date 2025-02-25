310f01c414560ac8cab641590f963487eeb17c09
Andres Gaviria <cgaviria930@gmail.com>
2025-02-24 20:26:13 -0500

    mejoras bento #1

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        'xxl': '1500px', // a partir de 1500px
+        // ... otras breakpoints
+      },
     },
   },
   plugins: [],
